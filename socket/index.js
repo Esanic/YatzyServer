3cc2af7b0fb48d2438a5f2d2dd32e437461b9cd1
Christopher Reineborn <christopher.reineborn@jsc.se>
2023-02-06 23:03:52 +0100

    Display of queue numbers on front page + refactoring

--- socket/index.js
+++ socket/index.js
@@ -1,14 +1,14 @@
-let playerTwosGame = [];
-let counterTwoPlayer = 0;
-let roomTwoPlayer = '[2]Game-'+counterTwoPlayer;
+let playerTwosQueue = [];
+let counterTwoPlayers = 0;
+let roomTwoPlayers = '[2]Game-'+counterTwoPlayers;
 
-let playerThreeGame = [];
-let counterThreePlayer = 0;
-let roomThreePlayer = '[3]Game-'+counterThreePlayer;
+let playerThreeQueue = [];
+let counterThreePlayers = 0;
+let roomThreePlayers = '[3]Game-'+counterThreePlayers;
 
-let playerFourGame = [];
-let counterFourPlayer = 0;
-let roomFourPlayer = '[4]Game-'+counterFourPlayer;
+let playerFourQueue = [];
+let counterFourPlayers = 0;
+let roomFourPlayers = '[4]Game-'+counterFourPlayers;
 
 
 
@@ -25,34 +25,24 @@ module.exports = (io) => {
         socket.emit('online', true)
 
 		socket.on('disconnect', () => {
-            let index = playerFourGame.findIndex(x => x.sid == socket.id);
+            let indexFourPlayer = playerFourQueue.findIndex(x => x.sid === socket.id);
+            let indexThreePlayer = playerThreeQueue.findIndex(x => x.sid === socket.id);
+            let indexTwoPlayer = playerTwosQueue.findIndex(x => x.sid === socket.id);
             
             //If user found in participantsFourGame
-            if(index !== -1){
-                playerFourGame.splice(index, 1);
-                io.emit('disconnectedInQueueFour', playerFourGame.length)
+            if(indexFourPlayer !== -1){
+                playerFourQueue.splice(indexFourPlayer, 1);
+                io.emit('disconnectedInQueueFour', playerFourQueue.length)
             }
-            
-            //If user not found in participantsFourGame
-            if(index === -1){
-                index = playerThreeGame.findIndex(x => x.sid === socket.id)
-
-                //If user found in participantsThreeGame
-                if(index !== -1){
-                    playerThreeGame.splice(index, 1);
-                    io.emit('disconnectedInQueueThree', playerThreeGame.length);
-                }
-                
-                //If user not found in participantsThreeGame
-                if(index === -1){
-                    index = playerTwosGame.findIndex(x => x.sid === socket.id);
-                    
-                    //If user found in participantsTwosGame
-                    if(index !== -1){
-                        playerTwosGame.splice(index, 1);
-                        io.emit('disconnectedInQueueTwos', playerTwosGame.length)
-                    }
-                }
+            //If user found in participantsThreeGame
+            if(indexThreePlayer !== -1){
+                playerThreeQueue.splice(indexThreePlayer, 1);
+                io.emit('disconnectedInQueueThree', playerThreeQueue.length);
+            }
+            //If user found in participantsTwosGame
+            if(indexTwoPlayer !== -1){
+                playerThreeQueue.splice(indexTwoPlayer, 1);
+                io.emit('disconnectedInQueueThree', playerThreeQueue.length);
             }
             
             io.emit('disconnected', socket.id);
@@ -62,88 +52,23 @@ module.exports = (io) => {
         
         //Join Room
         socket.on('joinRoom', (name, maxPlayers) => {
-            
-            //If user wants to join a two-player room
             if(maxPlayers === 2){
-                if(playerTwosGame.length <= 2){
-                    playerTwosGame.push({name: name, sid: socket.id});
-                    socket.join(roomTwoPlayer);
-                    console.log(socket.id, 'has joined', roomTwoPlayer)
-                    io.to(roomTwoPlayer).emit('amtOfPlayers', playerTwosGame.length)
-                    socket.emit('roomName', roomTwoPlayer);
-                    
-                    if(playerTwosGame.length === 2){
-                        io.to(roomTwoPlayer).emit('fullGame', true);
-                        io.to(roomTwoPlayer).emit('players', playerTwosGame);
-                        console.log(roomTwoPlayer, 'has started.')
-
-                        roomTwoPlayer = '[2]Game-'+(counterTwoPlayer++);
-                        playerTwosGame = []
-                    }
-                }
-                if(playerTwosGame.length > 2){
-                    roomTwoPlayer = '[2]Game-'+(counterTwoPlayer++);
-                    playerTwosGame = []
-                    socket.join(roomTwoPlayer);
-                    console.log(socket.id, 'has joined', roomTwoPlayer)
-                    playerTwosGame.push({name: name, sid: socket.id});
-                }
+                joinRoom(2, playerTwosQueue, name, socket, roomTwoPlayers, counterTwoPlayers);
             }
-
-            //If user wants to join a three-player room
             if(maxPlayers === 3){
-                if(playerThreeGame.length <= 3){
-                    playerThreeGame.push({name: name, sid: socket.id});
-                    socket.join(roomThreePlayer);
-                    console.log(socket.id, 'has joined', roomThreePlayer)
-                    io.to(roomThreePlayer).emit('amtOfPlayers', playerThreeGame.length)
-                    socket.emit('roomName', roomThreePlayer);
-                    
-                    if(playerThreeGame.length === 3){
-                        io.to(roomThreePlayer).emit('fullGame', true);
-                        io.to(roomThreePlayer).emit('players', playerThreeGame);
-                        console.log(roomThreePlayer, 'has started.')
-
-                        roomThreePlayer = '[3]Game-'+(counterThreePlayer++);
-                        playerThreeGame = []
-                    }
-                }
-                if(playerThreeGame.length > 3){
-                    roomThreePlayer = '[3]Game-'+(counterThreePlayer++);
-                    playerThreeGame = []
-                    socket.join(roomThreePlayer);
-                    playerThreeGame.push({name: name, sid: socket.id});
-                }
+                joinRoom(3, playerThreeQueue, name, socket, roomThreePlayers, counterThreePlayers);
             }
-
-            //If user wants to join a four-player room
             if(maxPlayers === 4){
-                if(playerFourGame.length <= 4){
-                    playerFourGame.push({name: name, sid: socket.id});
-                    socket.join(roomFourPlayer);
-                    console.log(socket.id, 'has joined', roomFourPlayer)
-                    io.to(roomFourPlayer).emit('amtOfPlayers', playerFourGame.length)
-                    socket.emit('roomName', roomFourPlayer);
-                    
-                    if(playerFourGame.length == 4){
-                        io.to(roomFourPlayer).emit('fullGame', true);
-                        io.to(roomFourPlayer).emit('players', playerFourGame);
-                        console.log(roomFourPlayer, 'has started.')
-
-                        roomFourPlayer = '[4]Game-'+(counterFourPlayer++);
-                        playerFourGame = []
-                    }
-                }
-                if(playerFourGame.length > 4){
-                    roomFourPlayer = '[4]Game-'+(counterFourPlayer++);
-                    playerFourGame = []
-                    socket.join(room);
-                    playerFourGame.push({name: name, sid: socket.id});
-                }
+                joinRoom(4, playerFourQueue, name, socket, roomFourPlayers, counterFourPlayers);
             }
         })
+
         socket.on('checkOnline', x => {
             socket.emit('online', true)
+        })
+
+        socket.on('triggerQueue', x => {
+            emitQueueNumbers();
         })
         
         //Dice Hit
@@ -160,6 +85,29 @@ module.exports = (io) => {
         socket.on('nextPlayer',(scoreRowName, dice, room) => {
             socket.broadcast.to(room).emit('getNextPlayer', {scoreRowName, dice})
         })
-	})
 
+        function emitQueueNumbers() {
+            io.emit('queueNumbers', {qTwo: playerTwosQueue.length, qThree: playerThreeQueue.length, qFour: playerFourQueue.length})
+        }
+
+        function joinRoom(numOfPlayers, queueArray, name, socket, room, counter){
+            if(queueArray.length <= numOfPlayers){
+                queueArray.push({name: name, sid: socket.id});
+                emitQueueNumbers();
+                socket.join(room);
+                console.log(socket.id, 'has joined', room);
+                io.to(room).emit('amtOfPlayers', queueArray.length)
+                socket.emit('roomName', room);
+                
+                if(queueArray.length === numOfPlayers){
+                    io.to(room).emit('fullGame', true);
+                    io.to(room).emit('players', queueArray);
+                    console.log(room, 'has started.')
+                    room = '['+counter+']Game-'+(counter++);
+                    queueArray.length = 0;
+                    emitQueueNumbers();
+                }
+            }
+        }
+	})
 }
